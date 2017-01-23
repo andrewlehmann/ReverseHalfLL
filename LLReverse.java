b4e230683ba5fbf0f119fd89898ad8280d1821b8
andrewlehmann <andrew@arlehmann.com>
2017-01-23 16:37:30 -0600

    Initial commit

---
+++ LLReverse.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class LLReverse	{
+
+	public static void main(String[] args)	{
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		// CREATE LIST
+		for(int i = 0; i < 8; i++)
+			list.add(i);
+
+		// SOLUTION
+		for(int i = list.size() / 2; i < list.size(); i++)	{
+			list.add(i, list.removeLast());
+		}
+		// DISPLAY
+		System.out.println(list);
+		System.exit(0);
+	}
+}
